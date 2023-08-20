4ab46ddc2398abadf6093dbe7858f5be4912eec7
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-20 13:14:44 +0200

    add motion wrapper in Hero

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -7,7 +7,12 @@ import { slideIn, staggerContainer, textVariant } from '../utils/motion';
 
 const Hero = () => (
   <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
-    Hero section
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{once: false, amount: 0.25 }}
+    />
   </section>
 );
 
