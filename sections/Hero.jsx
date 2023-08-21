4c00a9241d54ef5ff8b44ef6b843b03afff20871
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-21 11:02:42 +0200

    add hero header

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -12,7 +12,16 @@ const Hero = () => (
       initial="hidden"
       whileInView="show"
       viewport={{once: false, amount: 0.25 }}
-    />
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 variant={textVariant(1.1)}
+          className={styles.heroHeading}
+        >
+          Metaverse
+        </motion.h1>
+      </div>
+    </motion.div>
   </section>
 );
 
