05f1108420027b17196c8dc316c3b216b66e87cb
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-21 11:33:43 +0200

    add imports to About section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
     About section
   </section>
 );
