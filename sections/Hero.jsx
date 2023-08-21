a4f541ef639bb9c5eca2a852a0bf9e634cbb1a1a
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-21 11:19:23 +0200

    bugfix - heading in Hero

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -15,9 +15,7 @@ const Hero = () => (
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
       <div className="flex justify-center items-center flex-col relative z-10">
-        <motion.h1 variant={textVariant(1.1)}
-          className={styles.heroHeading}
-        >
+      <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
           Metaverse
         </motion.h1>
         <motion.div
@@ -33,6 +31,21 @@ const Hero = () => (
           </h1>
         </motion.div>
       </div>
+      <motion.div
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="relative w-full md:-mt-[20px] -mt-[12px]"
+      >
+        <div
+          className="absolute w-full h-[300px] hero-gradient rounded-top-[140px] z-[0] -top-[30px]"
+        >
+          <img 
+            src="/cover.png"
+            alt="cover"
+            className="w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative"
+          />
+        </div>
+      </motion.div>
+
     </motion.div>
   </section>
 );
