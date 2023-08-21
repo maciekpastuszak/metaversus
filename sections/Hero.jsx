3401409f2ac1fbe0716702b22b6f82f8f5237000
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-21 11:30:21 +0200

    fix on stamp icon

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => (
       >
         <div
           className="absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]"
-        >
+        />
           <img 
             src="/cover.png"
             alt="cover"
@@ -45,16 +45,15 @@ const Hero = () => (
           />
           <a href="#explore">
             <div className="w-full flex justify-end sm:-mt-[70px] -mt-[50px] pr-[40px] relative z-10">
-              <img 
+              <img
                 src="/stamp.png"
                 alt="stamp"
-                className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain" 
+                className="sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain"
               />
             </div>
           </a>
-        </div>
-      </motion.div>
 
+      </motion.div>
     </motion.div>
   </section>
 );
