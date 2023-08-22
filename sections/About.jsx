97485675342dd3432a1d5d4c465b6a5165c4cda4
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-22 08:41:23 +0200

    add gradient and motion wrapper to About

--- sections/About.jsx
+++ sections/About.jsx
@@ -8,7 +8,16 @@ import { fadeIn, staggerContainer } from '../utils/motion';
 
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
-    About section
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+
+    </motion.div>
   </section>
 );
 
