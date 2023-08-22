f28e2c49c5cf717181c6d3aeb26c185cd14b720e
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-22 08:57:42 +0200

    add About main text

--- sections/About.jsx
+++ sections/About.jsx
@@ -17,6 +17,13 @@ const About = () => (
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
       <TypingText title="| About Metaversus" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        Metaverse is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the madness of the metaverse of today, using only VR devices you can easily explore the metaverse world you want, turn your dreams into reality. Let's explore the madness of the metaverse by scrolling down
+      </motion.p>
     </motion.div>
   </section>
 );
