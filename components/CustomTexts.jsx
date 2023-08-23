9dc62a5d8ddd49a3c74965dcf83453fae8de3792
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-23 08:55:25 +0200

    update TitleText and header in Explore

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -20,10 +20,10 @@ export const TypingText = ({ title, textStyles }) => (
 
 export const TitleText = ({ title, textStyles }) => (
   <motion.h2
-  variants={textVariant2}
-  initial="hidden"
-  whileInView="show"
-  className={`mt-[9px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
     {title}
   </motion.h2>
