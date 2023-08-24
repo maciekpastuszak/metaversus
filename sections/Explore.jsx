6b67671a40efdc0f6c38c3a2c8cdba82b20762aa
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-24 10:39:06 +0200

    map through words and add hook for active

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -9,7 +9,10 @@ import { ExploreCard, TitleText, TypingText } from '../components';
 import { exploreWorlds } from '../constants';
 
 
-const Explore = () => (
+const Explore = () => {
+
+  const [active, setActive] = useState(true)
+  return (
   <section className={`${styles.paddings}`} id="explore">
     <motion.div
       variants={staggerContainer}
@@ -23,8 +26,19 @@ const Explore = () => (
         title={<>Choose the world you want <br className="md:block hidden" /> to explore</>}
         textStyles="text-center"
       />
+      <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
+        {exploreWorlds.map((world, index) => (
+          <ExploreCard
+            key={world.id}
+            {...world}
+            index={index}
+            active={active}
+            handleClick={setActive}
+          />
+        ))}
+      </div>
     </motion.div>
   </section>
-);
-
+  );
+}
 export default Explore;
