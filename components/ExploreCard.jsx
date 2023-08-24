e0e6b9ea19bffd1776b44b034c030f8b41d4ed47
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-24 10:53:08 +0200

    add imgs with worlds

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -13,7 +13,11 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
       active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
   >
-    Explore Card
+    <img
+      src={imgUrl}
+      alt={title}
+      className="absolute w-full h-full object-cover rounded-[24px]"
+    />
   </motion.div>
 );
 
