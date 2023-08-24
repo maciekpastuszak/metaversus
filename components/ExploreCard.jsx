dda7ea9190837a46466d509f0ddfc28bd23e7384
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-24 10:46:54 +0200

    add motion wrapper

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -7,9 +7,14 @@ import { fadeIn } from '../utils/motion';
 
 
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
-  <div>
+  <motion.div
+    variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
+    classaName={`relative ${
+      active === id ? 'lg:flex-[3.5] flex-[10]' : 'lg:flex-[0.5] flex-[2]'
+    } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
+  >
     Explore Card
-  </div>
+  </motion.div>
 );
 
 export default ExploreCard;
