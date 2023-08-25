34ae73fcc4085f8fdf5bfa6713a192f71a1b4fab
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-25 13:58:54 +0200

    add title of a card

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -34,6 +34,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
           Enter the Metaverse
         </p>
+        <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">{title}</h2>
       </div>
 
     )}
