2881c23241d2f08033a4f904ab5ddd7ba3f5c171
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-27 21:54:06 +0200

    typo

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -24,7 +24,7 @@ const GetStarted = () => (
         <img 
         src="/get-started.png"
           alt="get-started"
-          className="w-[90%] h-[90%] objject-contain"
+          className="w-[90%] h-[90%] object-contain"
         />
       </motion.div>
     </motion.div>
