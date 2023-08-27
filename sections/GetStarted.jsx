7228269e922c1771557cac1bf463d66930e7c313
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-27 21:58:36 +0200

    map through startingFeatures to show StartingSteps

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -33,6 +33,15 @@ const GetStarted = () => (
       >
         <TypingText title="| How Metaversus Works" />
         <TitleText title={<>Get started with just a few clicks</>} />
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {startingFeatures.map((feature, index) => (
+            <StartSteps
+              key={feature}
+              number={index + 1}
+              text={feature}
+            />
+          ))}
+        </div>
       </motion.div>
     </motion.div>
   </section>
