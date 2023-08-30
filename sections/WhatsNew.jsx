e65503601adeb56cdc3bd6e6dd1038b5c4bf8329
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-08-30 23:06:26 +0200

    correct position of the text and planet

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -16,16 +16,7 @@ const WhatsNew = () => (
     viewport={{ once: false, amount: 0.25 }}
     className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
   >
-    <motion.div
-      variants={planetVariants('left')}
-      className={`flex-1 ${styles.flexCenter}`}
-    >
-      <img 
-      src="/get-started.png"
-        alt="get-started"
-        className="w-[90%] h-[90%] object-contain"
-      />
-    </motion.div>
+    
     <motion.div
       variants={fadeIn('left', 'tween', 0.2, 1)}
       className="flex-[0.75] flex justify-center flex-col"
@@ -41,6 +32,16 @@ const WhatsNew = () => (
           />
         ))}
       </div>
+    </motion.div>
+    <motion.div
+      variants={planetVariants('right')}
+      className={`flex-1 ${styles.flexCenter}`}
+    >
+      <img 
+      src="/whats-new.png"
+        alt="get-started"
+        className="w-[90%] h-[90%] object-contain"
+      />
     </motion.div>
   </motion.div>
 </section>
