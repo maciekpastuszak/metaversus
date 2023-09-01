9950302c95b8f3bf59067f1af2f23742646110ca
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-09-01 23:19:14 +0200

    add section wrapper and motion div in World

--- sections/World.jsx
+++ sections/World.jsx
@@ -8,8 +8,15 @@ import { fadeIn, staggerContainer } from '../utils/motion';
 
 
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+    </motion.div>
   </section>
 );
 
