5a2c15168fb610cf50dd46688433976a00809ae5
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-09-01 18:54:43 +0200

    add NewFeatures wrapper

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,9 +1,12 @@
 import styles from '../styles';
 
 const NewFeatures = ({ imgUrl, title, subtitle }) => (
-  <div>
-    New Features
+  <div className="flex-1 flex flex-col sm:max-w-[250px] min-w-[210px]">
+  <div
+    className={`${styles.flexCenter} w-[70px] h-[70px] rounded-[24px] bg-[#323F5D]`}
+  >
   </div>
+</div>
 );
 
 export default NewFeatures;
