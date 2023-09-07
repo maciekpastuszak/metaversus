4eca699df0d28b490fa083ab0d2d7b39615f9c03
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-09-07 22:33:38 +0200

    map through insights in the Insight section

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -16,8 +16,13 @@ const Insights = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex flex-col`}
     >
-      <TypingText title="Insight" textStyles="text-center" />
+      <TypingText title="| Insight" textStyles="text-center" />
       <TitleText title="Insight about Metaverse" textStyles="text-center"/>
+      <div className="mt-[50px] flex flex-col gap-[30px]">
+        {insights.map((insight, index) => (
+          <InsightCard key={`insight-${index}`} {...item} index={index + 1} />
+        ))}
+      </div>
     </motion.div>
   </section>
 );
