3f5743e521231011821b0c279afb373ff43739c1
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-09-07 22:41:55 +0200

    typo

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -5,14 +5,14 @@ import { fadeIn } from "../utils/motion";
 
 const InsightCard = ({imgUrl, title, subtitle, index}) => (
   <motion.div
-    variant={fadeIn('up', 'spring', index * 0.5,1)}
-    className="flex md:flex-row flex-col gap-4"
-  >
-    <img 
-      src={imgUrl}
-      alt="planet-01"
-      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
-    />
+  variants={fadeIn('up', 'spring', index * 0.5, 1)}
+  className="flex md:flex-row flex-col gap-4"
+>
+  <img
+    src={imgUrl}
+    alt="planet-01"
+    className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+  />
   </motion.div>
 );
 
