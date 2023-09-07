86f70eb3644d0b254b27072dc98cfb952f4ecf80
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-09-07 22:35:07 +0200

    add import and props to InsightCard

--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -1,6 +1,9 @@
 'use client';
 
-const InsightCard = () => (
+import { motion } from "framer-motion";
+import { fadeIn } from "../utils/motion";
+
+const InsightCard = ({imgUrl, title, subtitle, index}) => (
   <div>
     Insight Card
   </div>
