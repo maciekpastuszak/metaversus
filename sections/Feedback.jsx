7c4ed91b6f44fea6482437d6c5e809dd9c291c1a
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-09-08 22:00:03 +0200

    add imports and wrapper to Feedback section

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -1,8 +1,21 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
+
+
 const Feedback = () => (
-  <section>
-    Feedback section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+    </motion.div>
   </section>
 );
 
