f5dc989b0a3e7dc1c4c8a21fd8f46497baa9f257
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-09-14 12:44:13 +0200

    add footer subtitle

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -15,6 +15,14 @@ const Footer = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="footer-gradient" />
+
+    <div className={`${styles.innerWidth} mx-auto flex flex-col gap-8`}>
+      <div className="flex items-center justify-between flex-wrap gap-5">
+        <h4 className="font-bold md:text-[64px] text-[44px] text-white">
+          Enter the Metaverse
+        </h4>
+      </div>
+    </div>
   </motion.footer>
 );
 
